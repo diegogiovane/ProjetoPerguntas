e9353328e550b9eadcb6c824c288d246e5aef8ff
Diego Giovane Nogueira Duarte <diego.giovane@gmail.com>
2024-08-20 23:39:09 -0300

    37. Variáveis em StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,21 @@ void main(List<String> args) =>	runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
 	PerguntaApp({super.key});
-	final perguntas = [
-		'Qual é sua cor favorita?',
-		'Qual é seu animal favorito?'
-	];
+
+	var perguntaSelecionada = 0;
 
 	void responder() {
-		debugPrint('Pergunta respondida #01');
-	}
-
-	void Function() funcaoQueRetornOutraFuncao() {
-		return () {
-			debugPrint('Pergunta respondida #02');
-		};
+		perguntaSelecionada++;
+		debugPrint(perguntaSelecionada.toString());
 	}
 
 	@override
 	Widget build(BuildContext context) {
+		final perguntas = [
+			'Qual é sua cor favorita?',
+			'Qual é seu animal favorito?'
+		];
+
 		return MaterialApp(
 			home: Scaffold(
 				appBar: AppBar(
@@ -28,24 +26,18 @@ class PerguntaApp extends StatelessWidget {
 				),
 				body: Column(
 					children: <Widget>[
-				    	Text(perguntas[0]),
+				    	Text(perguntas[perguntaSelecionada]),
 						ElevatedButton(
 							onPressed: responder,
 							child: const Text('Resposta 1')
 						),
 						ElevatedButton(
-							onPressed: funcaoQueRetornOutraFuncao(),
+							onPressed: responder,
 							child: const Text('Resposta 2')
 						),
 						ElevatedButton(
-							onPressed: () {
-								debugPrint('Pergunta respondida #03');
-							},
+							onPressed: responder,
 							child: const Text('Resposta 3')
-						),
-						ElevatedButton(
-							onPressed: () => debugPrint('Pergunta respondida #04'),
-							child: const Text('Resposta 4')
 						)
 					],
 				),
